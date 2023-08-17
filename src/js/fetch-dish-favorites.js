40ee896ded39d05f598410f61f82a7bb86b1a2e1
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-08-18 01:20:43 +0300

    update

--- src/js/fetch-dish-favorites.js
+++ src/js/fetch-dish-favorites.js
@@ -1,9 +1,33 @@
 // Підключення бібліотеки
 import axios from 'axios';
 //========================
+import {markupCardFavorites} from './markup-card-favorites.js'
 
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
-export async function fetchDishFavorites(ID) {
+async function fetchDishFavorites(ID) {
   const response = await axios.get(`${BASE_URL}/recipes/${ID}`);
   return response.data;
 }
+
+const dishIds = [
+  '6462a8f74c3d0ddd28897fb8',
+  '6462a8f74c3d0ddd28897fba',
+  '6462a8f74c3d0ddd28897fbb',
+];
+
+const fetchDishes = async () => {
+  try {
+    // 1. Створюємо масив промісів
+    const arrayOfPromises = dishIds.map(async dishId => {
+      const response = await fetchDishFavorites(dishId);
+      return response;
+    });
+    // 2. Запускаємо усі проміси паралельно і чекаємо на їх завершення
+    const dishArr = await Promise.all(arrayOfPromises);
+    markupCardFavorites(dishArr);
+    return dishArr;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+fetchDishes();
--- src/js/markup-card-favorites.js
+++ src/js/markup-card-favorites.js
@@ -1,97 +1,13 @@
-import { fetchDishFavorites } from './fetch-dish-favorites.js';
+const dishListEl = document.querySelector('.favorites-cards');
 
-// async function objDataForMarkup(idDish) {
-//   try {
-//     const data = await fetchDishFavorites(idDish);
-//     const objData = {
-//       id: `${data._id}`,
-//       img: `${data.preview}`,
-//       title: `${data.title}`,
-//       description: `${data.description}`,
-//       category: `${data.category}`,
-//       rating: `${data.rating}`,
-//     };
-//     return objData;
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-
-// async function markupCardFavorites(idDish) {
-//   try {
-//     const objData = await objDataForMarkup(idDish);
-//     const { id, img, title, description, rating } = objData;
-//     const markup = `<li>
-//           <button>
-//               <svg aria-label="heart" class="icon-star icon">
-//                   <use href="./img/icons.svg#icon-star"></use>
-//               </svg>
-//           </button>
-//           <img src="${img}" alt="${title}">
-//           <div>
-//               <p></p>
-//               <p></p>
-//               <div>
-//                   <div>
-//                       <svg aria-label="heart" class="icon-star icon">
-//                           <use href="./img/icons.svg#icon-star"></use>
-//                       </svg>
-//                       <svg aria-label="heart" class="icon-star icon">
-//                           <use href="./img/icons.svg#icon-star"></use>
-//                       </svg>
-//                       <svg aria-label="heart" class="icon-star icon">
-//                           <use href="./img/icons.svg#icon-star"></use>
-//                       </svg>
-//                       <svg aria-label="heart" class="icon-star icon">
-//                           <use href="./img/icons.svg#icon-star"></use>
-//                       </svg>
-//                       <svg aria-label="heart" class="icon-star icon">
-//                           <use href="./img/icons.svg#icon-star"></use>
-//                       </svg>
-//                   </div>
-//                   <button></button>
-//               </div>
-//           </div>
-//       </li>`;
-//     console.log(markup);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-
-// markupCardFavorites('6462a8f74c3d0ddd28897fb8');
-//=================================================
-
-const dishIds = [
-  '6462a8f74c3d0ddd28897fb8',
-  '6462a8f74c3d0ddd28897fba',
-  '6462a8f74c3d0ddd28897fbb',
-];
-
-const fetchUsers = async () => {
-  try {
-    // 1. Створюємо масив промісів
-    const arrayOfPromises = dishIds.map(async dishId => {
-      const response = await fetchDishFavorites(dishId);
-      return response;
-    });
-    // 2. Запускаємо усі проміси паралельно і чекаємо на їх завершення
-    const dishArr = await Promise.all(arrayOfPromises);
-    markupCardFavorites(dishArr);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-fetchUsers();
-
-function markupCardFavorites(dishArr) {  
+export function markupCardFavorites(dishArr) {
   const markup = dishArr
     .map(
       ({ _id, preview, title, description, rating }) =>
         `<li>
             <button>
-                <svg aria-label="heart" class="icon-star icon">
-                    <use href="./img/icons.svg#icon-star"></use>
+                <svg aria-label="heart" class="icon-heart icon">
+                    <use href="./img/icons.svg#icon-heart"></use>
                 </svg>
             </button>
             <img src="${preview}" alt="${title}">
@@ -101,19 +17,19 @@ function markupCardFavorites(dishArr) {
                 <div>
                     <div>
                         <span>${rating}</span>
-                        <svg aria-label="heart" class="icon-star icon">
+                        <svg aria-label="star" class="icon-star icon" width=18 height=18>
                             <use href="./img/icons.svg#icon-star"></use>
                         </svg>
-                        <svg aria-label="heart" class="icon-star icon">
+                        <svg aria-label="star" class="icon-star icon">
                             <use href="./img/icons.svg#icon-star"></use>
                         </svg>
-                        <svg aria-label="heart" class="icon-star icon">
+                        <svg aria-label="star" class="icon-star icon">
                             <use href="./img/icons.svg#icon-star"></use>
                         </svg>
-                        <svg aria-label="heart" class="icon-star icon">
+                        <svg aria-label="star" class="icon-star icon">
                             <use href="./img/icons.svg#icon-star"></use>
                         </svg>
-                        <svg aria-label="heart" class="icon-star icon">
+                        <svg aria-label="star" class="icon-star icon">
                             <use href="./img/icons.svg#icon-star"></use>
                         </svg>
                     </div>
@@ -123,45 +39,18 @@ function markupCardFavorites(dishArr) {
         </li>`
     )
     .join('');
-    userList.innerHTML = markup;
+  dishListEl.insertAdjacentHTML('beforeend', markup);
+  localStorageSet(dishArr);
 }
 
-//===============
 
-// const fetchUsersBtn = document.querySelector(".btn");
-// const userList = document.querySelector(".user-list");
+function localStorageSet(dishArr){
+    console.log(dishArr);
+    localStorage.setItem("dishArr", `${dishArr}`);
+}
 
-// fetchUsersBtn.addEventListener("click", async () => {
-//   try {
-//     const users = await fetchUsers();
-//     renderUserListItems(users);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// });
-
-// async function fetchUsers() {
-//   const baseUrl = "https://jsonplaceholder.typicode.com";
-//   const userIds = [1, 2, 3, 4, 5];
-
-//   const arrayOfPromises = userIds.map(async (userId) => {
-//     const response = await fetch(`${baseUrl}/users/${userId}`);
-//     return response.json();
-//   });
-
-//   const users = await Promise.all(arrayOfPromises);
-//   return users;
-// }
-
-// function renderUserListItems(users) {
-//   const markup = users
-//     .map(
-//       (user) => `<li class="item">
-//         <p><b>Name</b>: ${user.name}</p>
-//         <p><b>Email</b>: ${user.email}</p>
-//         <p><b>Company</b>: ${user.company.name}</p>
-//       </li>`
-//     )
-//     .join("");
-//   userList.innerHTML = markup;
-// }
+function localStorageGet(){
+    const local = localStorage.getItem("dishArr");
+    console.log(local);
+}
+localStorageGet()
