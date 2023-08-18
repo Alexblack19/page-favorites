36e9d802f23a48adf079f0234e1c598f00473d01
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-08-18 15:33:27 +0300

    update

--- src/js/markup-card-favorites.js
+++ src/js/markup-card-favorites.js
@@ -1,41 +1,39 @@
 const dishListEl = document.querySelector('.favorites-cards');
 
 export function markupCardFavorites(dishArr) {
-    console.log(dishArr);
+  console.log(dishArr);
   const markup = dishArr
     .map(
       ({ _id, preview, thumb, title, description, rating }) =>
         `<li class="fav-card item">
             <button class="fav-card-heart-btn">
                 <svg aria-label="heart" class="fav-card-heart icon">
-                    <use href="./img/icons.svg#icon-heart"></use>
+                    <use href="./img/icons.svg#icon-astronaut"></use>
                 </svg>
             </button>
-            <img class="fav-card-img" src="${thumb}" alt="${title}">
-            <div сlass-"fav-card-info">
-                <p class="fav-card-title">${title}</p>
-                <p class="fav-card-desc">${description}</p>
+            <img class="fav-card-img" src="${thumb}" alt="${title}">            
+            <h3 class="fav-card-title">${title}</h3>
+            <p class="fav-card-desc">${description}</p>
+            <div сlass="fav-card-info-wrap">
                 <div>
-                    <div>
-                        <span class="fav-card-rating">${rating}</span>
-                        <svg aria-label="star" class="fav-card-star icon">
-                            <use href="./img/icons.svg#icon-star"></use>
-                        </svg>
-                        <svg aria-label="star" class="fav-card-star icon">
-                            <use href="./img/icons.svg#icon-star"></use>
-                        </svg>
-                        <svg aria-label="star" class="fav-card-star icon">
-                            <use href="./img/icons.svg#icon-star"></use>
-                        </svg>
-                        <svg aria-label="star" class="fav-card-star icon">
-                            <use href="./img/icons.svg#icon-star"></use>
-                        </svg>
-                        <svg aria-label="star" class="fav-card-star icon">
-                            <use href="./img/icons.svg#icon-star"></use>
-                        </svg>
-                    </div>
-                    <button class="fav-card-see-btn">See recipe</button>
+                    <span class="fav-card-rating">${rating}</span>
+                    <svg aria-label="star" class="fav-card-star icon">
+                        <use href="./img/icons.svg#icon-star"></use>
+                    </svg>
+                    <svg aria-label="star" class="fav-card-star icon">
+                        <use href="./img/icons.svg#icon-star"></use>
+                    </svg>
+                    <svg aria-label="star" class="fav-card-star icon">
+                        <use href="./img/icons.svg#icon-star"></use>
+                    </svg>
+                    <svg aria-label="star" class="fav-card-star icon">
+                        <use href="./img/icons.svg#icon-star"></use>
+                    </svg>
+                    <svg aria-label="star" class="fav-card-star icon">
+                        <use href="./img/icons.svg#icon-star"></use>
+                    </svg>
                 </div>
+                <button class="fav-card-see-btn">See recipe</button>
             </div>
         </li>`
     )
@@ -44,14 +42,13 @@ export function markupCardFavorites(dishArr) {
   localStorageSet(dishArr);
 }
 
-
-function localStorageSet(dishArr){
-    console.log(dishArr);
-    localStorage.setItem("dishArr", `${dishArr}`);
+function localStorageSet(dishArr) {
+  console.log(dishArr);
+  localStorage.setItem('dishArr', `${dishArr}`);
 }
 
-function localStorageGet(){
-    const local = localStorage.getItem("dishArr");
-    console.log(local);
+function localStorageGet() {
+  const local = localStorage.getItem('dishArr');
+  console.log(local);
 }
-localStorageGet()
+localStorageGet();
