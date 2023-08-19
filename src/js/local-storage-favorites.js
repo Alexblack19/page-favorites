0f7005f03fa1347f6244633e8bda76344b3b354a
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-08-19 19:05:00 +0300

    Update local-storage-favorites.js

--- src/js/local-storage-favorites.js
+++ src/js/local-storage-favorites.js
@@ -1,16 +1,17 @@
 import { markupCardFavorites } from './markup-card-favorites.js';
 
 export function localStorageSet(dishArrBack) {
-  const arrLocStorAdd = localStorageGet();
-  dishArrBack.forEach(element => {
-    arrLocStorAdd.push(element);
-  });
-  localStorage.setItem('dishLocalKey', JSON.stringify(arrLocStorAdd));
-  console.log(arrLocStorAdd);
+  if (dishArrBack !== []) {
+    const arrLocStorAdd = localStorageGet();
+    dishArrBack.forEach(element => {
+      arrLocStorAdd.push(element);
+    });
+    localStorage.setItem('dishLocalKey', JSON.stringify(arrLocStorAdd));
+  }
+
   if (dishArrBack !== []) {
     markupCardFavorites(dishArrBack);
   }
-  return;
 }
 
 export function localStorageGet() {
