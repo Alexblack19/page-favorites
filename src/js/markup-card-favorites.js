401f7699cafe4fd982f6abd4643ffed1d8cf259c
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-08-19 17:51:00 +0300

    Update markup-card-favorites.js

--- src/js/markup-card-favorites.js
+++ src/js/markup-card-favorites.js
@@ -10,36 +10,36 @@ export function markupCardFavorites(dishArr) {
     .map(
       ({ _id, preview, thumb, title, description, rating }) =>
         `<li id=${_id} class="fav-card item">
-            <button class="fav-card-heart-btn">
-                <svg aria-label="heart" class="fav-card-heart icon" viewBox="0 0 32 32">
-                  <path opacity="1"  d="M15.992 6.848c-2.666-3.117-7.111-3.955-10.451-1.101s-3.81 7.625-1.187 11c2.181 2.806 8.781 8.725 10.944 10.641 0.242 0.214 0.363 0.321 0.504 0.364 0.123 0.037 0.258 0.037 0.381 0 0.141-0.042 0.262-0.149 0.504-0.364 2.163-1.916 8.763-7.834 10.944-10.641 2.623-3.375 2.21-8.177-1.187-11.001s-7.785-2.015-10.451 1.101z"></path>
-                </svg>
-            </button>
-            <img class="fav-card-img" src="${thumb}" alt="${title}">
-            <h3 class="fav-card-title">${title}</h3>
-            <p class="fav-card-desc">${description}</p>
-            <div class="fav-card-info-wrap">
-                <div class="fav-rating-wrap">
-                    <span class="fav-card-rating">${rating}</span>
-                    <svg aria-label="star" class="fav-card-star icon" viewBox="0 0 32 32">
-                    ${patchStar}
-                    </svg>
-                    <svg aria-label="star" class="fav-card-star icon" viewBox="0 0 32 32">
-                    ${patchStar}
-                    </svg>
-                    <svg aria-label="star" class="fav-card-star icon" viewBox="0 0 32 32">
-                    ${patchStar}
-                    </svg>
-                    <svg aria-label="star" class="fav-card-star icon" viewBox="0 0 32 32">
-                    ${patchStar}
-                    </svg>
-                    <svg aria-label="star" class="fav-card-star icon" viewBox="0 0 32 32">
-                    ${patchStar}
-                    </svg>
-                </div>
-                <button class="fav-card-see-btn">See recipe</button>
-            </div>            
-        </li>`
+                                  <button class="fav-card-heart-btn">
+                                      <svg aria-label="heart" class="fav-card-heart icon" viewBox="0 0 32 32">
+                                        <path opacity="1"  d="M15.992 6.848c-2.666-3.117-7.111-3.955-10.451-1.101s-3.81 7.625-1.187 11c2.181 2.806 8.781 8.725 10.944 10.641 0.242 0.214 0.363 0.321 0.504 0.364 0.123 0.037 0.258 0.037 0.381 0 0.141-0.042 0.262-0.149 0.504-0.364 2.163-1.916 8.763-7.834 10.944-10.641 2.623-3.375 2.21-8.177-1.187-11.001s-7.785-2.015-10.451 1.101z"></path>
+                                      </svg>
+                                  </button>
+                                  <img class="fav-card-img" src="${thumb}" alt="${title}">
+                                  <h3 class="fav-card-title">${title}</h3>
+                                  <p class="fav-card-desc">${description}</p>
+                                  <div class="fav-card-info-wrap">
+                                      <div class="fav-rating-wrap">
+                                          <span class="fav-card-rating">${rating}</span>
+                                          <svg aria-label="star" class="fav-card-star icon" viewBox="0 0 32 32">
+                                          ${patchStar}
+                                          </svg>
+                                          <svg aria-label="star" class="fav-card-star icon" viewBox="0 0 32 32">
+                                          ${patchStar}
+                                          </svg>
+                                          <svg aria-label="star" class="fav-card-star icon" viewBox="0 0 32 32">
+                                          ${patchStar}
+                                          </svg>
+                                          <svg aria-label="star" class="fav-card-star icon" viewBox="0 0 32 32">
+                                          ${patchStar}
+                                          </svg>
+                                          <svg aria-label="star" class="fav-card-star icon" viewBox="0 0 32 32">
+                                          ${patchStar}
+                                          </svg>
+                                      </div>
+                                      <button class="fav-card-see-btn">See recipe</button>
+                                  </div>            
+                              </li>`
     )
     .join('');
   dishListEl.insertAdjacentHTML('beforeend', markup);
@@ -77,6 +77,6 @@ function onSeeRecipeClick(e) {
 }
 
 // Рендерінг сторінки після перезавантаження //
-window.addEventListener('pageshow', function () {
+window.addEventListener('pageshow', function (e) {
   markupCardFavorites(localStorageGet());
 });
